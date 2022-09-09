d3b7b55589d22ad45bc9128a2aeb4068fb2fa9ce
Ethan <ethanbaird94@gmail.com>
2022-09-09 12:13:59 +0100

    adds Pilot

--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,19 +1,21 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
 
     private Pilot pilot;
 
     @Before
     public void before(){
-        pilot - new Pilot("Ethan", Rank.CAPTAIN, "LN100");
+        pilot = new Pilot("Ethan", Rank.CAPTAIN, "LN100");
     }
 
     @Test
     public void hasRank(){
         Rank rank = pilot.getRank();
-        int actual = rank.getValue()();
+        int actual = rank.getValue();
         assertEquals(1, actual);
     }
 
