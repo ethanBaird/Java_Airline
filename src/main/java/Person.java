cf210fe9865a7e041f11a7ea7057e03e6a5dd4bc
Ethan <ethanbaird94@gmail.com>
2022-09-09 11:32:52 +0100

    hasName() pass

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,12 @@
 public class Person {
+
+    protected String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
