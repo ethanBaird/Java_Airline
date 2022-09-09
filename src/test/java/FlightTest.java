3fe2f611717e60035b8b3f3b2d0034044073da70
Ethan <ethanbaird94@gmail.com>
2022-09-09 12:53:44 +0100

    adds FlightTest wiht before()

---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+public class FlightTest {
+
+    private ArrayList<Pilot> testPilots;
+    private ArrayList<CabinCrewMember> testCabinCrewMembers;
+    private ArrayList<Passenger> testPassengers;
+
+    private Pilot pilot;
+    private CabinCrewMember cabinCrewMember;
+    private Passenger passenger;
+
+    private Plane plane;
+
+    private Flight flight;
+
+    @Before
+    public void before(){
+        testPilots = new ArrayList<>();
+        testCabinCrewMembers = new ArrayList<>();
+        testPassengers = new ArrayList<>();
+
+        pilot = new Pilot("Ethan", Rank.CAPTAIN, "LN100");
+        cabinCrewMember = new CabinCrewMember("Rachel", Rank.FLIGHTATTENDANT);
+        passenger = new Passenger("Minnie", 2);
+
+        testPilots.add(pilot);
+        testCabinCrewMembers.add(cabinCrewMember);
+        testPassengers.add(passenger);
+
+        plane = new Plane(PlaneType.BOEING747);
+
+        flight = new Flight(testPilots, testCabinCrewMembers, testPassengers, plane, "FN123", "GLA", "EDB", "0700");
+    }
+
+}
